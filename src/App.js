819d798512a208b6edd672971845270af44aed19
jaygajera-hub <gajerajay9@gmail.com>
2022-06-01 14:36:27 +0530

    n

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+
+export default class App extends Component {
+  c='john';
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <News/>
+      </div>
+    )
+  }
+}
+
+
+
